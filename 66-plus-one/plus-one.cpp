646e4965774b0bc4c649466accf8ae7ff248ed92
aarushi2715 <aarushisinghnaruka@gmail.com>
2026-08-13 23:56:40 +0530

    Time: 3 ms (5.64%) | Memory: 11.5 MB (62.00%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i=digits.size()-1; i>=0; i--){
+            //if digits are less than 9 then increment and return 
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
             }
+            //if it is = 9 then 
             digits[i] = 0;
         }
+        //if every digit is 9
         digits.insert(digits.begin(),1);
         return digits;
         
